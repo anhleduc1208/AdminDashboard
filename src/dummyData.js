1c68bfab466f0c7b38eeefe5f950ab8a7bb89060
anhleduc12081995 <anh.leduc@gigantec.media>
2022-10-27 06:41:54 +0700

    small change 1
    small change 2
    
    small change 3

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,208 +1,201 @@
-import pic1 from './picture.jpeg';
+import pic1 from "./picture.jpeg";
 
 export const userData = [
-    {
-      name: 'Jan',
-      "Active User": 4000,
-    },
-    {
-      name: 'Feb',
-      "Active User": 1200,
-    },
-    {
-      name: 'Mar',
-      "Active User": 3200,
-    },
-    {
-      name: 'Apr',
-      "Active User": 5200,
-    },
-    {
-      name: 'May',
-      "Active User": 2400,
-    },
-    {
-      name: 'Jun',
-      "Active User": 4888,
-    },
-    {
-      name: 'Jul',
-      "Active User": 2041,
-    },
-    {
-      name: 'Agu',
-      "Active User": 100,
-    },
-    {
-      name: 'Sep',
-      "Active User": 900,
-    },
-    {
-      name: 'Oct',
-      "Active User": 1500,
-    },
-    {
-      name: 'Nov',
-      "Active User": 4000,
-    },
-    {
-      name: 'Dec',
-      "Active User": 3000,
-    },
-    
+	{
+		name: "Jan1",
+		"Active User": 4000,
+	},
+	{
+		name: "Feb 2",
+		"Active User": 1200,
+	},
+	{
+		name: "Mar 3",
+		"Active User": 3200,
+	},
+	{
+		name: "Apr",
+		"Active User": 5200,
+	},
+	{
+		name: "May",
+		"Active User": 2400,
+	},
+	{
+		name: "Jun",
+		"Active User": 4888,
+	},
+	{
+		name: "Jul",
+		"Active User": 2041,
+	},
+	{
+		name: "Agu",
+		"Active User": 100,
+	},
+	{
+		name: "Sep",
+		"Active User": 900,
+	},
+	{
+		name: "Oct",
+		"Active User": 1500,
+	},
+	{
+		name: "Nov",
+		"Active User": 4000,
+	},
+	{
+		name: "Dec",
+		"Active User": 3000,
+	},
 ];
 
 export const productData = [
-  {
-    name: 'Jan',
-    "Sales": 4000,
-  },
-  {
-    name: 'Feb',
-    "Sales": 1200,
-  },
-  {
-    name: 'Mar',
-    "Sales": 3200,
-  },
- 
-  
-  
+	{
+		name: "Jan",
+		Sales: 4000,
+	},
+	{
+		name: "Feb",
+		Sales: 1200,
+	},
+	{
+		name: "Mar",
+		Sales: 3200,
+	},
 ];
 
-
 export const userRows = [
-  { 
-      id: 1, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 2, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 3, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 4, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 5, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 6, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 7, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-  { 
-      id: 8, 
-      username: 'Jon Snow', 
-      avatar: pic1, 
-      email: 'jon@gmail.com',
-      status: 'active',
-      transaction: '$120.00',
-  },
-
+	{
+		id: 1,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 2,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 3,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 4,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 5,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 6,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 7,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
+	{
+		id: 8,
+		username: "Jon Snow",
+		avatar: pic1,
+		email: "jon@gmail.com",
+		status: "active",
+		transaction: "$120.00",
+	},
 ];
 
 export const productRows = [
-  { 
-      id: 1, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 2, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 3, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 4, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 5, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 6, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 7, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-  { 
-      id: 8, 
-      name: 'Apple Airpods', 
-      img: pic1, 
-      stock: 123,
-      status: 'active',
-      price: '$120.00',
-  },
-
+	{
+		id: 1,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 2,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 3,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 4,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 5,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 6,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 7,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
+	{
+		id: 8,
+		name: "Apple Airpods",
+		img: pic1,
+		stock: 123,
+		status: "active",
+		price: "$120.00",
+	},
 ];
